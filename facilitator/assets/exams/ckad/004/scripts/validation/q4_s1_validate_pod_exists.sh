8ae76ed2b6922656795453f859bfc393723a0325
josephedward <josephedwardwork@gmail.com>
2025-12-18 14:38:25 -0500

    Update q4_s1_validate_pod_exists.sh

--- facilitator/assets/exams/ckad/004/scripts/validation/q4_s1_validate_pod_exists.sh
+++ facilitator/assets/exams/ckad/004/scripts/validation/q4_s1_validate_pod_exists.sh
@@ -2,7 +2,7 @@
 # Q04.01 - Pod logger-pod exists
 # Points: 2
 
-kubectl get pod logger-pod -n logging >/dev/null 2>&1 && {
+kubectl get pod logger-pod -n sidecar-logging >/dev/null 2>&1 && {
   echo "✓ Pod logger-pod exists"
   exit 0
 } || {
